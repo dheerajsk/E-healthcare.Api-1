9a9825ba380368626693cf63e732d2aab34ef722
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2022-08-30 08:07:42 +0530

    Update BaseEntity.cs

--- ProjectManagement.Entities/BaseEntity.cs
+++ ProjectManagement.Entities/BaseEntity.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace EHealthcare.Entities
 {
     public class BaseEntity
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long ID { get; set; }
     }
 }
